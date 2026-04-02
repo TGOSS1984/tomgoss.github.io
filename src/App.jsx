15a3b527d235ff39fe09fd3a41f1ddc826c0fc34
TGOSS1984 <tomgoss84@gmail.com>
2026-04-02 16:04:03 +0100

    Set up React Router with initial portfolio page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Journey from "./pages/Journey";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <div>
-      <h1>Tom Goss Portfolio</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/journey" element={<Journey />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
