dc3213ecf5e67dab6c6f83dea4f879ceda53da12
TGOSS1984 <tomgoss84@gmail.com>
2026-04-05 18:37:47 +0100

    Fix: Remove duplicate router from main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <App />
 );
